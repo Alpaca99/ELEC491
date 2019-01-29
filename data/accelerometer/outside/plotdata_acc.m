f9088f5fe6c1981344f563edf15352c9c2b31aa9
Emilyyyyyy <xiongjingwei1996@gmail.com>
2019-01-28 16:34:34 -0800

    Update plotdata_acc.m

--- data/accelerometer/outside/plotdata_acc.m
+++ data/accelerometer/outside/plotdata_acc.m
@@ -13,4 +13,20 @@ plot(X,'r');
 hold on
 plot(Y,'b');
 plot(Z,'g');
-legend('x','y','z');
+hold on
+
+
+fileID = fopen("75cm90deg_outside.txt");
+D = textscan(fileID,'%f %f %f');
+fclose(fileID);
+a=D{1}
+b=D{2}
+c=D{3}
+n=10000;
+p_end=3;
+p=0:p_end/n:p_end*(1-1/n);
+%data_float=data* 0.004882814;
+%plot(t,data);
+plot(a);
+plot(b);
+plot(c);
